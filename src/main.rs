041ca7620902626aa6df997374c29e236a70b8cb
Snopx <258700581@qq.com>
2021-07-07 00:27:38 +0800

    tcp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    // ...
+
+    println!("new client incoming");
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8099")?;
+    println!("listening on :8099");
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
