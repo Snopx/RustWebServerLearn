502ea0b876dc264880ce91878617ae62c8eda0e1
Snopx <258700581@qq.com>
2021-07-11 13:40:53 +0800

    single thread simple web server

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
 
-fn handle_client(stream: TcpStream) {
-    // ...
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    println!("new client incoming");
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8099")?;
-    println!("listening on :8099");
+    println!("listening on :{}", listener.local_addr().unwrap());
     // accept connections and process them serially
     for stream in listener.incoming() {
         handle_client(stream?);
